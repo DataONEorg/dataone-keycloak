e2614142eaeb65930196a7658f1b57f9f0064b99
Matt Jones <gitcode@magisa.org>
2026-01-07 00:33:56 -0900

    Created functional version of the app.
    ORCID login works but has many warts, mainly in dealing with the proxy
    server from k8s ingress, and being sure redirect_uri is working. Turns out
    I had to set the OIDC client redirect_uri to use http and not https for
    it to work.

--- pythonapp/app.py
+++ pythonapp/app.py
@@ -1,15 +1,30 @@
+'''
+A simple Flask application demonstrating OpenID Connect (OIDC) authentication
+using the flask-oidc library against a keycloak server. The application includes 
+routes for logging in and accessing protected resources.
+'''
+
+import os
 from flask_oidc import OpenIDConnect
 from flask import Flask, session, g
 
+
 app = Flask(__name__)
+
+params = {}
+params['FLASK_SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', os.urandom(32).hex())
+
 app.config.update({
-    'SECRET_KEY': '4ac081c6-9e80-11f0-afb5-4e1c2cda7276',  # Replace with a strong secret key
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',  # Path to your OIDC client configuration
-    'OIDC_SCOPES': ['openid', 'email', 'profile'],  # Requested OIDC scopes
-    'OIDC_REDIRECT_URI': 'https://api.test.dataone.org/kctest', # Your application's redirect URI
-    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
+    'OIDC_CLIENT_SECRETS': './client_secrets.json',
+    # 'OIDC_OVERWRITE_REDIRECT_URI': 'https://api.test.dataone.org/kctest/authorize',
+    # 'OIDC_CALLBACK_ROUTE': '/kctest/authorize',
+    'SECRET_KEY': params['FLASK_SECRET_KEY']
 })
+
 oidc = OpenIDConnect(app)
+
+# Works: https://auth.test.dataone.org/auth/realms/DataONE/protocol/openid-connect/auth?client_id=ogdc&response_type=code&redirect_uri=https://api.test.dataone.org/kctest/authorize
+# Returns code to: https://api.test.dataone.org/kctest/authorize?session_state=b2161565-3560-4805-b514-12ff88377301&iss=https%3A%2F%2Fauth.test.dataone.org%2Fauth%2Frealms%2FDataONE&code=7abdbc78-6202-4e9d-bf59-ab228ed5f17f.b2161565-3560-4805-b514-12ff88377301.8483d161-ff76-4eb1-9d98-5cfe424e6458
 
 @app.route('/kctest')
 def index():
@@ -18,7 +33,7 @@ def index():
         return f"Hello, {user_info.get('email')}!"
         # return 'Welcome %s' % session["oidc_auth_profile"].get('email')
     else:
-        return 'Welcome! Please <a href="/login">login</a>.'
+        return 'Welcome! Please <a href="/kctest/login">login</a>.'
 
 
 @app.route('/kctest/login')
@@ -36,4 +51,7 @@ def alternative():
         return 'Not logged in'
 
 if __name__ == "__main__":
+    print(oidc.client_secrets)
+    print(oidc.accept_token)
+    print(params['FLASK_SECRET_KEY'])
     app.run(host="0.0.0.0", port=int("4000"), debug=True)
