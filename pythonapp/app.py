685d0d7c27279dd5775593aa8ee139a3162ff39c
Matt Jones <gitcode@magisa.org>
2026-01-07 02:38:10 -0900

    Add details to user profile info page

--- pythonapp/app.py
+++ pythonapp/app.py
@@ -23,17 +23,14 @@ app.config.update({
 
 oidc = OpenIDConnect(app)
 
-# Works: https://auth.test.dataone.org/auth/realms/DataONE/protocol/openid-connect/auth?client_id=ogdc&response_type=code&redirect_uri=https://api.test.dataone.org/kctest/authorize
-# Returns code to: https://api.test.dataone.org/kctest/authorize?session_state=b2161565-3560-4805-b514-12ff88377301&iss=https%3A%2F%2Fauth.test.dataone.org%2Fauth%2Frealms%2FDataONE&code=7abdbc78-6202-4e9d-bf59-ab228ed5f17f.b2161565-3560-4805-b514-12ff88377301.8483d161-ff76-4eb1-9d98-5cfe424e6458
-
 @app.route('/kctest')
 def index():
     if oidc.user_loggedin:
         user_info = oidc.user_getinfo(['email', 'profile'])
-        return f"Hello, {user_info.get('email')}!"
+        return f"<h3>Hello, {user_info.get('email')}!</h3> <br> <p>Please visit the <a href='/kctest/info'>user info page</a> to see more details.</p>"
         # return 'Welcome %s' % session["oidc_auth_profile"].get('email')
     else:
-        return 'Welcome! Please <a href="/kctest/login">login</a>.'
+        return '<h3>Welcome!</h3> Please <a href="/kctest/login">login</a>.'
 
 
 @app.route('/kctest/login')
@@ -42,16 +39,28 @@ def login():
     return 'Welcome %s' % session["oidc_auth_profile"].get('email')
 
 
-@app.route('/kctest/alt')
-def alternative():
+@app.route('/kctest/info')
+def user_info():
     # This uses the user instance at g.oidc_user instead
     if g.oidc_user.logged_in:
-        return 'Welcome %s' % g.oidc_user.profile.get('email')
+        # Profile from g.oidc_user
+        profile_list_1 = "<ul>\n" + "\n".join([f"  <li><strong>{k}:</strong> {v}</li>" for k, v in g.oidc_user.profile.items()]) + "\n</ul>"
+
+        # Profile from session['oidc_auth_profile']
+        profile = session["oidc_auth_profile"]
+        profile_list_2 = ("<ul>\n" + 
+                          "\n".join([f"  <li><strong>{k}:</strong> {v}</li>" for k, v in profile.items()]) + 
+                          "\n</ul>")
+
+        name = profile['name']
+        access_token = g.oidc_user.access_token
+        refresh_token = g.oidc_user.refresh_token
+        groups = g.oidc_user.groups
+        unique_id = g.oidc_user.unique_id
+
+        return f"<h3>Hello, {name}!</h3> <br> <ul> <li><strong>Access token:</strong> {access_token} </li> <li><strong>Refresh token:</strong> {refresh_token} </li> <li><strong>Groups:</strong> {groups} </li> <li><strong>Unique ID:</strong> {unique_id} </li> </ul> <br> <h3>Profile from g.oidc_user:</h3> {profile_list_1} <br> <h3>Profile from session['oidc_auth_profile']:</h3> {profile_list_2}"
     else:
-        return 'Not logged in'
+        return '<h3>Not logged in</h3>'
 
 if __name__ == "__main__":
-    print(oidc.client_secrets)
-    print(oidc.accept_token)
-    print(params['FLASK_SECRET_KEY'])
     app.run(host="0.0.0.0", port=int("4000"), debug=True)
